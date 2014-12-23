42555dba9f1a3443d7d6e015ad18353ed6546c5e
Brett <bmgeorg@g.clemson.edu>
2014-12-23 17:18:54 -0500

    Create storyboard for archived questions. No code yet implemented.

---
+++ Master/ArchiveQuestionCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArchiveQuestionCell.swift
+//  Master
+//
+//  Created by Brett George on 12/23/14.
+//  Copyright (c) 2014 Brett George. All rights reserved.
+//
+
+import UIKit
+
+class ArchiveQuestionCell: UITableViewCell {
+    
+    @IBOutlet weak var questionTitle: UILabel!
+    @IBOutlet weak var index: UILabel!
+    @IBOutlet weak var checkmarkIndicator: UIButton!
+}
