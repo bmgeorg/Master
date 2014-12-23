dd04a225bfef3ff34787da842717d6e3d13a1073
Brett <bmgeorg@g.clemson.edu>
2014-12-23 15:54:48 -0500

    Layout main menu

---
+++ Master/MainMenuViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MainMenuViewController.swift
+//  Master
+//
+//  Created by Brett George on 12/23/14.
+//  Copyright (c) 2014 Brett George. All rights reserved.
+//
+
+import UIKit
+
+class MainMenuViewController: UIViewController {
+    
+}
