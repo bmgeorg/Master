aa02e0191a5c23f4301b55f947136b731cdce85d
Brett <bmgeorg@g.clemson.edu>
2014-12-23 23:31:29 -0500

    Create interface for question view.

---
+++ Master/QuestionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  QuestionViewController.swift
+//  Master
+//
+//  Created by Brett George on 12/23/14.
+//  Copyright (c) 2014 Brett George. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+}
