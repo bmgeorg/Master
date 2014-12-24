0146ae23145fb9aff4af516cb65efdab634726ea
Brett <bmgeorg@g.clemson.edu>
2014-12-24 01:46:29 -0500

    Add close and hide keyboard functionality. Scroll answer field into view on showing keyboard.

--- Master/QuestionViewController.swift
+++ Master/QuestionViewController.swift
@@ -8,24 +8,22 @@
 
 import UIKit
 
-class QuestionViewController: UIViewController {
+class QuestionViewController: UIViewController, UITextFieldDelegate, UIGestureRecognizerDelegate {
     @IBOutlet weak var questionTitle: UINavigationItem!
     @IBOutlet weak var questionLabel: UILabel!
     @IBOutlet weak var explanationLabel: UILabel!
     @IBOutlet weak var answerField: UITextField!
     @IBOutlet weak var enterButton: UIButton!
-    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     var question: Question!
+    
+    let ANIMATION_DURATION: NSTimeInterval = 1
+    let BOTTOM_MARGIN: CGFloat = 8
     
     internal func configureView() {
         questionTitle.title = question.title
         questionLabel.text = question.question
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        configureView()
     }
     
     @IBAction func checkAnswer() {
@@ -36,5 +34,60 @@ class QuestionViewController: UIViewController {
             explanationLabel.text = "Wrong"
             explanationLabel.textColor = UIColor.redColor()
         }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillShow:"), name:UIKeyboardWillShowNotification, object: nil);
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("keyboardWillHide:"), name:UIKeyboardWillHideNotification, object: nil);
+        answerField.delegate = self
+        addDismissKeyboardRecognizer()
+    }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self);
+    }
+    
+    // Mark: Keyboard Handling
+    
+    func keyboardWillShow(notification: NSNotification) {
+        let info = notification.userInfo!
+        let keyboardFrame = (info[UIKeyboardFrameEndUserInfoKey] as NSValue).CGRectValue()
+        let contentInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardFrame.height, 0.0);
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+        
+        var frame = CGRectOffset(answerField.frame, 0, BOTTOM_MARGIN)
+        self.scrollView.scrollRectToVisible(frame, animated: true)
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        let contentInsets = UIEdgeInsetsZero
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+    //close keyboard on return press
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        checkAnswer()
+        return false
+    }
+    
+    //close keyboard tap outside of keyboard, except when tap is on enterButton (see gestureRecognizer: shouldReceiveTouch: method)
+    internal func addDismissKeyboardRecognizer() {
+        let tapDismiss = UITapGestureRecognizer(target: self, action:"dismissKeyboard")
+        tapDismiss.cancelsTouchesInView = false
+        tapDismiss.delegate = self
+        self.view.addGestureRecognizer(tapDismiss)
+    }
+    
+    //do not close keyboard when tap is on enter button
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldReceiveTouch touch: UITouch) -> Bool {
+        return !touch.view.isDescendantOfView(enterButton)
+    }
+    
+    internal func dismissKeyboard() {
+        answerField.resignFirstResponder()
     }
 }
