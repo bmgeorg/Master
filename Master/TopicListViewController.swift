f1ba09572c685681056e0aae49d72aaf62024a95
Brett <bmgeorg@g.clemson.edu>
2014-12-26 12:15:13 -0500

    Create interface for main menu and topic list. No code yet.

---
+++ Master/TopicListViewController.swift
@@ -0,0 +1,32 @@
+//
+//  TopicListViewController.swift
+//  Master
+//
+//  Created by Brett George on 12/26/14.
+//  Copyright (c) 2014 Brett George. All rights reserved.
+//
+
+import UIKit
+import Realm
+
+class TopicListViewController: UITableViewController {
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("TopicCell", forIndexPath: indexPath) as UITableViewCell
+        return cell
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if(segue.identifier == "") {
+        } else {
+            assert(false, "Unrecognized segue identifier from ArchiveViewController")
+        }
+    }
+}
