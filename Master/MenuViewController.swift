900000cd436e2eda8ffe181b8489cd9b133cf144
Brett <bmgeorg@g.clemson.edu>
2015-01-01 10:59:36 -0500

    Change title of back button to menu.

---
+++ Master/MenuViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MenuViewController.swift
+//  Master
+//
+//  Created by Brett George on 1/1/15.
+//  Copyright (c) 2015 Brett George. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+    override func viewDidLoad() {
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: UIBarButtonItemStyle.Bordered, target: nil, action: nil)
+    }
+}
